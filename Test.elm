41f261d2463dc52f2c27ecf566e7081f5b2eba70
Alexander Dinu <aluuu@husa.su>
2015-09-05 18:46:36 +0500

    Add travis integration

--- Test.elm
+++ Test.elm
@@ -8,57 +8,94 @@ import Check.Investigator exposing ( Investigator
                                    , shrink
                                    , random
                                    , lowerCaseChar
-                                   , upperCaseChar
-                                   )
-import Check.Runner.Browser exposing (display)
+                                   , upperCaseChar )
 
-import Draft.Date as D exposing (Date)
--- import Draft.Time as Time
--- import Draft.DateTime as DateTime
+import Draft.Date as Date exposing (Date(..), fromJulianDay)
+import Draft.Time as Time exposing (Time(Milliseconds), fromMilliseconds)
 import Shrink
 import String
 import Random.Extra as Random
+import Check.Runner.IO exposing (display)
+
+import IO.IO exposing (..)
+import IO.Runner exposing (Request, Response)
+import IO.Runner as IO
 
 date : Investigator Date
 date =
   let
-    shrinker {julianDay} =
-      Shrink.map (\d -> {julianDay=(abs d)}) (shrink int julianDay)
+    shrinker (Days julianDay) =
+      Shrink.map (\d -> fromJulianDay (abs d)) (shrink int julianDay)
 
-    generator =
-      Random.map (\d -> {julianDay=(abs d)}) (random int)
+    generator = Random.map (\d -> fromJulianDay (abs d)) (random int)
   in
     investigator generator shrinker
 
+time : Investigator Time
+time =
+  let
+    shrinker (Milliseconds ms) =
+      Shrink.map (\ms -> fromMilliseconds (abs ms)) (shrink int ms)
+
+    generator = Random.map (\ms -> fromMilliseconds (abs ms)) (random int)
+  in
+    investigator generator shrinker
 
 dateSuite =
   suite "Draft.Date"
-    [ claim "day number in ordinal date is between 1 and 366"
+    [ claim "Day number in ordinal date is between 1 and 366"
     `true`
-      (\d -> let (y, yd) = D.toOrdinalDate d in 1 <= yd && yd <= 366)
+      (\d -> let (y, yd) = Date.toOrdinalDate d in 1 <= yd && yd <= 366)
     `for`
       date
 
-    , claim "month day number is between 1 and 31"
+    , claim "Month day number is between 1 and 31"
     `true`
-      (\d -> let md = D.day d in 1 <= md && md <= 31)
+      (\d -> let md = Date.day d in 1 <= md && md <= 31)
     `for`
         date
 
-    , claim "month number is between 1 and 12"
+    , claim "Month number is between 1 and 12"
     `true`
-      (\d -> let m = D.month d in 1 <= m && m <= 12)
+      (\d -> let m = Date.month d in 1 <= m && m <= 12)
     `for`
         date
 
-    , claim "year number should be greater or equal than 1858"
+    , claim "Year number should be greater or equal than 1858"
     `true`
-      (\d -> let y = D.year d in 1858 <= y)
+      (\d -> let y = Date.year d in 1858 <= y)
     `for`
       date ]
 
-result =
-    quickCheck dateSuite
 
-main =
-    display result
+timeSuite =
+  suite "Draft.Time"
+    [ claim "Hours number is in range from 0 to 23"
+    `true`
+      (\t -> let h = Time.hours t in 0 <= h && h <= 23)
+    `for`
+      time
+    , claim "Minutes number is in range from 0 to 59"
+    `true`
+      (\t -> let h = Time.minutes t in 0 <= h && h <= 59)
+    `for`
+      time
+    , claim "Seconds number is in range from 0 to 59"
+    `true`
+      (\t -> let h = Time.seconds t in 0 <= h && h <= 59)
+    `for`
+      time ]
+
+testSuite = suite "Draft" [dateSuite, timeSuite]
+
+result = quickCheck testSuite
+
+run : IO ()
+run =
+  display result >>= \success ->
+    exit (if success then 0 else 1)
+
+port responses : Signal Response
+
+port requests : Signal Request
+port requests = IO.run responses run
